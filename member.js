dc3afdb2c88ca183ea3fa8dedf15d23eb1d048ac
czuwara-dxc <139341884+czuwara-dxc@users.noreply.github.com>
2023-07-27 09:30:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/skills/member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMember'
+  };
+}
